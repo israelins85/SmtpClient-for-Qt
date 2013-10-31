031200225db78619d17063089edd0d1b6175dd70
olehs <olehs@i.ua>
2013-10-31 14:16:06 -0700

    define for static linking

--- src/smtpmime_global.h
+++ src/smtpmime_global.h
@@ -1,10 +1,14 @@
 #ifndef SMTPMIME_GLOBAL_H
 #define SMTPMIME_GLOBAL_H
 
+#ifdef SMTP_MIME_STATIC
+#define SMTP_MIME_EXPORT
+#else
 #ifdef SMTP_MIME_LIBRARY
 #define SMTP_MIME_EXPORT Q_DECL_EXPORT
 #else
 #define SMTP_MIME_EXPORT Q_DECL_IMPORT
+#endif
 #endif
 
 #endif // SMTPMIME_GLOBAL_H
